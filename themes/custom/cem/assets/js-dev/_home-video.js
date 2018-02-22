f1aabf0a5ac18c48c630bf5c02f5bec25cf6f610
Daniel Marcio de Medeiros <Daniel Marcio de Medeiros>
2018-02-22 14:51:18 -0300

    menu home

---
+++ themes/custom/cem/assets/js-dev/_home-video.js
@@ -0,0 +1,16 @@
+(function(window, document, $) {
+    'use strict';
+    if ($('.home-video').length) {
+        $('.home-video div.home-video-box div.inner').slick({
+            autoplay: true,
+            autoplaySpeed: 6000,
+            dots: true,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            fade: true
+        });
+    }
+})(window, document, jQuery);
