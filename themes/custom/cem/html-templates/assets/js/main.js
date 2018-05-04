55ec519b2b9fc3ebaafc90c3bde03c33434e2a89
danielmme <medeiros.danielm@gmail.com>
2018-05-03 21:45:59 -0300

    versao 5

--- themes/custom/cem/html-templates/assets/js/main.js
+++ themes/custom/cem/html-templates/assets/js/main.js
@@ -81,6 +81,21 @@
         });
     }
 })(window, document, jQuery);
+(function(window, document, $) {
+    'use strict';
+    if ($('.resolution-slider').length) {
+        $('.resolution-slider div.views-element-container>div').slick({
+            autoplay: true,
+            autoplaySpeed: 6000,
+            dots: true,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false
+        });
+    }
+})(window, document, jQuery);
 /*(function (window, document, $) {
     'use strict';
 })(window, document, jQuery);*/
