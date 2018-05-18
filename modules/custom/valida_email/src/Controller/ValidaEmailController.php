dcd9f1a971f5d3d14335e07132a078a66b782f7c
Daniel Marcio de Medeiros <Daniel Marcio de Medeiros>
2018-05-18 17:26:40 -0300

    módulo customizado teste

--- modules/custom/valida_email/src/Controller/ValidaEmailController.php
+++ modules/custom/valida_email/src/Controller/ValidaEmailController.php
@@ -3,6 +3,8 @@
 namespace Drupal\valida_email\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 /**
 * An example controller.
@@ -12,11 +14,21 @@ class ValidaEmailController extends ControllerBase {
 /**
 * Returns a render-able array for a test page.
 */
-public function content() {
-$build = [
-'#markup' => t('olá sou a página que vai validar o email do usuário!'),
-];
-return $build;
-}
+  public function content(Request $request) {
+    // Create an object of type Select
+    $connection = \Drupal::database();
+    $query = $connection->select('webform_submission_data', 'webform');
+  
+    // Add extra detail to this query object: a condition, fields and a range
+    $query->condition('webform.name', 'e_mail', '=');
+    $query->fields('webform', ['value']);
+    $result = $query->execute();
+    foreach ($result as $record) {
+      $data = $record->value;
+    }
+    $response['data'] = $data;
+    $response['method'] = 'GET';
+  return new JsonResponse( $response );
+  }
 
 }
