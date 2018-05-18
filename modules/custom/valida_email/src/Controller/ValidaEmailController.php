2eaaebad0e9afc67cd1f6d23ccbef9dbf22879f8
Daniel Marcio de Medeiros <Daniel Marcio de Medeiros>
2018-05-18 16:31:08 -0300

    módulo customizado

---
+++ modules/custom/valida_email/src/Controller/ValidaEmailController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Drupal\example\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+* An example controller.
+*/
+class ValidaEmailController extends ControllerBase {
+
+/**
+* Returns a render-able array for a test page.
+*/
+public function content() {
+$build = [
+'#markup' => t('olá sou a página que vai validar o email do usuário!'),
+];
+return $build;
+}
+
+}
