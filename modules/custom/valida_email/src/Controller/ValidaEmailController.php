797748dcf2b43401687a26f3024f76fed807ef0f
DanielMM <medeiros.danielm@gmail.com>
2018-05-19 17:36:12 -0300

    controle de acesso

--- modules/custom/valida_email/src/Controller/ValidaEmailController.php
+++ modules/custom/valida_email/src/Controller/ValidaEmailController.php
@@ -5,30 +5,46 @@ namespace Drupal\valida_email\Controller;
 use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 /**
 * An example controller.
 */
 class ValidaEmailController extends ControllerBase {
 
-/**
-* Returns a render-able array for a test page.
-*/
   public function content(Request $request) {
-    // Create an object of type Select
-    $connection = \Drupal::database();
-    $query = $connection->select('webform_submission_data', 'webform');
-  
-    // Add extra detail to this query object: a condition, fields and a range
-    $query->condition('webform.name', 'e_mail', '=');
-    $query->fields('webform', ['value']);
-    $result = $query->execute();
-    foreach ($result as $record) {
-      $data = $record->value;
-    }
-    $response['data'] = $data;
-    $response['method'] = 'GET';
-  return new JsonResponse( $response );
+
+      if(isset($_POST['email']) && !empty($_POST['email'])){
+
+        $connection = \Drupal::database();
+        $query = $connection->select('webform_submission_data', 'webform');
+        $query->condition('webform.name', 'e_mail', '=');
+        $query->condition('webform.value', $_POST['email'], '=');
+        $query->fields('webform', ['value']);
+        $result = $query->execute();
+        foreach ($result as $record) {
+          if(!empty($record->value)){
+              $data = TRUE;
+             // $value = $session->get('usuario_validado', 'default');
+          }else{
+              $session = \Drupal::request()->getSession();
+              $session->set('usuario_validado', 'sim');
+              $data = FALSE;
+          }
+        }
+      }else {
+          $data = FALSE;
+      }
+      $response['retorno'] = $data;
+      return new JsonResponse( $response );
   }
+
+    public function form() {
+        return [
+            '#theme' => 'my_template'
+        ];
+    }
+
+
 
 }
