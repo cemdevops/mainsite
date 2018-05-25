2b7276dfb1c3997f1ec1fe58a041cf6143b28279
DanielMM <medeiros.danielm@gmail.com>
2018-05-24 21:02:03 -0300

    novas funcionalidades modulo customizado

--- modules/custom/valida_email/js/script.js
+++ modules/custom/valida_email/js/script.js
@@ -1,11 +1,31 @@
 (function($) {
     $(document).ready(function(){
+
         $('#formulario_controle').submit(function(){
+            var dados = $( this ).serialize();
+            var nid  = $("#id_curso").val();
+            console.log(nid);
+            $.ajax({
+                type: "POST",
+                url: "valida_email",
+                data: dados,
+                success: function( data ) {
+                    if (data.retorno == null) {
+                        window.location.href = '/form/cadastro-curso';
+                    }else{
+                        window.location.href = '/node/'+nid;
+                    }
+                }
+            });
+            return false;
+        });
+
+        $('#webform-submission-cadastro-curso-add-form').submit(function(){
             var dados = $( this ).serialize();
 
             $.ajax({
                 type: "POST",
-                url: "valida_email",
+                url: "configura_sessao",
                 data: dados,
                 success: function( data ) {
                     console.log(data);
