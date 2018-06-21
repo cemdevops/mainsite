12f9a33cf9c552526b592e1525bc6d6fbb9b081e
DanielMM <medeiros.danielm@gmail.cm>
2018-06-21 16:55:03 -0300

    csv import

--- modules/custom/csv_import/src/Controller/csv_importController.php
+++ modules/custom/csv_import/src/Controller/csv_importController.php
@@ -4,102 +4,63 @@ namespace Drupal\csv_import\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\webform\Entity\Webform;
+use Drupal\webform\Entity\WebformSubmission;
 use Drupal\webform\WebformSubmissionForm;
 use \Symfony\Component\HttpFoundation\Response;
-use Drupal\webform\Entity;
-use Drupal\webform\Entity\WebformSubmission;
 use ParseCsv\Csv;
 
-class webformimportController extends ControllerBase{
-    
-    public function import_webform_csv(){
-      
-        $csv = new Csv('tabela_usuarios_cem4.csv');
-//        $response = array();
-//        foreach($csv->data as $values) {
-//          $response[] = $values['nome'];
-//        }
-        
-//        $filePath = \Drupal::service('file_system')->realpath(file_default_scheme() . "://");
-//        $filename = $filePath.'/rsvp_form.csv';
-//        $handle = fopen($filename, 'r');
-        //kint($handle);
-//        $arrSubmission = array();
-//        while (($rowData = fgetcsv($handle)) !== FALSE) {
-//            //kint($rowData);
-//            $response = $this->add_rsvp_submission($rowData);
-//            //kint($response);
-//            break;
-//        }
-//        fclose($handle);
 
-        $response = print_r($csv->data);
-        return new Response(render($response));
-    }
+
+class csv_importController extends ControllerBase {
+  
+  public function import_webform_csv() {
     
-    public function add_rsvp_submission($rowData = array()){
-        
-        foreach($rowData as $data){
-            $wedformID[] = $data;
-        }
-        //add to webform and send edm
-        $responseWF = false;
-        $response = array();
-        $values = null;
-        //set the values
-        $values = [
-            'webform_id' => $wedformID[0],
-            'entity_type' => $wedformID[1],
-            'entity_id' => $wedformID[2],
-            'in_draft' => $wedformID[3],
-            'uid' => $wedformID[4],
-            'langcode' => $wedformID[5],
-            'token' => $wedformID[6],
-            'uri' => '/webform/rsvp_form/api',
-            'remote_addr' => '',
-            'data' => [
-              'name' => $wedformID[7],
-              'email' => $wedformID[8],
-              'contact_no' => $wedformID[9],
-              'terms_and_conditions' => ($wedformID[10] == 'Yes') ? 1 : 0,
-            ],
-        ];
-        
-        // Check webform is open.
-        $webform = Webform::load($values['webform_id']);
-        //kint($webform);
-        if($webform != null){
-            //check the webform is open to submit
-            $is_open = WebformSubmissionForm::isOpen($webform);
-            
-            if ($is_open === TRUE) {
-              // Validate submission.
-                $errors = WebformSubmissionForm::validateValues($values);
-                
-                // Check there are no validation errors.
-                if (!empty($errors)) {
-                    //kint($values);
-                    //kint($errors);
-                    $responseWF = false;
-                }
-                else {
-                    // Submit values and get submission ID.
-                    $webform_submission = WebformSubmissionForm::submitValues($values);
-                    //kint($webform_submission);
-                    if(is_numeric($webform_submission->id()) &&  $webform_submission->id() > 0){
-                        $responseWF = true;
-                        $response["response"] = "success";
-                    }
-                }
-            }
-        }
-        
-        if($responseWF === false){  
-            $response["response"] = "Submission failed. Please contact the site administrator.";
-        }
-        unset($wedformID);
-        return $response;
+    $filePath = \Drupal::service('file_system')
+      ->realpath(file_default_scheme() . "://");
+    $filename = $filePath . '\tabela_usuarios_cem4.csv';
+    $csv = new Csv($filename);
+    $webform_id = 'cadastro_curso';
+    $values = array();
+    foreach($csv->data as $value) {
+      $values[] = [
+        'webform_id' => $webform_id,
+        'entity_type' => NULL,
+        'entity_id' => NULL,
+        'in_draft' => FALSE,
+        'uid' => 1,
+        'langcode' => 'pt-br',
+        'token' => 'O56qkNgoP9IOH6ohhYriW7SI3rgZdCnhEZFRkJqvZUE',
+        'uri' => '/pt-br/webform/cadastro_curso/api',
+        'remote_addr' => '::1',
+        'data' => array(
+          'nome' => $value['nome'],
+          'e_mail' => $value['email'],
+          'instituicao' => $value['instituicao'],
+          'cidade' => $value['municipio'],
+        ),
+      ];
     }
+    // Check webform is open.
+    $webform = Webform::load($webform_id);
+    if ($webform != NULL) {
+      $is_open = WebformSubmissionForm::isOpen($webform);
+      if ($is_open === TRUE) {
+        // Submit values and get submission ID.
+        foreach($values as $var) {
+          $errors = \Drupal\webform\WebformSubmissionForm::validateFormValues($var);
+          // Check there are no validation errors.
+          if(empty($errors)) {
+            $webform_submission = \Drupal\webform\WebformSubmissionForm::submitFormValues($var);
+          }
+        }
+      }
+    }
+    $response["response"] = "success";
+    return new Response($response);
+  }
+  
+  
+  
 }
 
 
