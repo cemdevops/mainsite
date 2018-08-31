2de1b3033bd033ac284e707ebd2aec7d5fffee43
DanielMM <medeiros.danielm@gmail.cm>
2018-08-31 17:23:32 -0300

    changes

--- modules/custom/csv_import/src/Controller/csv_importController.php
+++ modules/custom/csv_import/src/Controller/csv_importController.php
@@ -62,8 +62,7 @@ class csv_importController extends ControllerBase {
   }
   
   public function import_base_dados() {
-  
-  
+    
     function getTidByName($name = NULL, $vid = NULL) {
       $properties = [];
       if (!empty($name)) {
@@ -79,59 +78,75 @@ class csv_importController extends ControllerBase {
     
       return !empty($term) ? $term->id() : 0;
     }
-  
-  
-    $term_name = getTidByName('BASE DE DADOS', 'tags');
-  
-  
-    $filePath = \Drupal::service('file_system')
-      ->realpath(file_default_scheme() . "://");
-    $base_de_dados = $filePath . '\documents.csv';
-  
-   
-    $csv = new Csv($base_de_dados);
-  
-//    $csv->encoding('UTF-8', 'UTF-8');
-    $csv->delimiter = ";";
-  
-    $h = fopen($base_de_dados, "r");
-    while (($data = fgetcsv($h, 100000, ";")) !== FALSE)
-    {
-  
-      kint($data);
-    }
-
-    die();
     
-      foreach($csv->data as $value) {
-        $filename = $filePath . '/arquivos-para-migrar/vcnapuc.jpg';
-        $file = file_save_data($filename, "public://vcnapuc.png", FILE_EXISTS_REPLACE);
+    $filePath = \Drupal::service('file_system')->realpath(file_default_scheme() . "://");
+    $base_de_dados = $filePath . "\documents6.csv";
+    $h =fopen($base_de_dados, "r");
+    
+    while (($data = fgetcsv($h, 100000, ";")) !== FALSE) {
+      $base[] = $data;
+    }
+    fclose($h);
+//    kint($base);
+//    foreach ($base as $value){
+//      kint($value[10]);
+//    }
+//    die();
+    $head = array_shift($base);
+    $tag   = array();
+    $fonte = array();
+    $tema  = array();
+    $tipo  = array();
+    
+      foreach($base as $value) {
+        $filename = $filePath . "\arquivos-para-migrar&#92" . $value[8];
+        $file = file_save_data($filename, 'public://' . $value[8], FILE_EXISTS_REPLACE);
+        
         $node = Node::create(['type' => 'documentos']);
-        $node->set('title', $value['titulo']);
-      
+        $node->set('title', $value[1]);
         $body = [
-          'value' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor',
+          'value' => $value[5],
           'format' => 'basic_html',
         ];
         $node->set('body', $body);
         $node->set('uid', 1);
         $documentos[] = [
           'target_id' => $file->id(),
-          'alt' => 'teste do teste do teste',
-          'title' => 'vamos ver isso',
+          'description' => $value[7],
         ];
         $node->set('field_documento', $documentos);
-        $node->set('field_documento_data_ref', 'Ut enim ad minima veniam');
-        $tags = [118, 123, 122];
-        getTidByName('BASE DE DADOS', 'tags');
-        $fontes = [];
-        $temas = [];
-        $tipos = [];
-        $node->set('field_documento_tags', $tags);
-        $node->set('field_documento_fontes', $fontes);
-        $node->set('field_documento_temas', $temas);
-        $node->set('field_documento_tags', $tags);
-        $node->set('field_documento_data_lancamento', $value['data_lancamento']);
+        $node->set('field_documento_data_ref', $value[9]);
+        $node->set('field_documento_data_lancamento', $value[10]);
+//        $tags = [118, 123, 122];
+  
+        $tags   = explode('#', $value[4]);
+        $fontes = explode('#', $value[3]);
+        $temas  = explode('#', $value[2]);
+        $tipos  = explode('#', $value[6]);
+  
+        foreach($tags as $value){
+          $tag[] = getTidByName($value, 'tags_base_dados');
+        }
+        foreach($fontes as $value){
+          $fonte[] = getTidByName($value, 'fontes');
+        }
+        foreach($temas as $value){
+          $tema[] = getTidByName($value, 'temas');
+        }
+        foreach($tipos as $value){
+          $tipo[] = getTidByName($value, 'tipos');
+        }
+   
+        $node->set('field_documento_tags', $tag);
+        $node->set('field_documento_fontes', $fonte);
+        $node->set('field_documento_temas', $tema);
+        $node->set('field_documento_tipos', $tipo);
+
+//        $node->set('field_documento_data_lancamento', '1981-01-17');
+        $tag   = array();
+        $fonte = array();
+        $tema  = array();
+        $tipo  = array();
         $node->status = 1;
         $node->langcod = 'pt-br';
         $node->enforceIsNew();
@@ -145,6 +160,115 @@ class csv_importController extends ControllerBase {
       ];
     
     }
+  
+  public function import_mapoteca() {
+    
+    function getTidByName($name = NULL, $vid = NULL) {
+      $properties = [];
+      if (!empty($name)) {
+        $properties['name'] = $name;
+      }
+      if (!empty($vid)) {
+        $properties['vid'] = $vid;
+      }
+      $terms = \Drupal::entityManager()
+        ->getStorage('taxonomy_term')
+        ->loadByProperties($properties);
+      $term = reset($terms);
+      
+      return !empty($term) ? $term->id() : 0;
+    }
+    
+    $filePath = \Drupal::service('file_system')->realpath(file_default_scheme() . "://");
+    $base_de_dados = $filePath . "\mapoteca.csv";
+    $h =fopen($base_de_dados, "r");
+    
+    while (($data = fgetcsv($h, 100000, ";")) !== FALSE) {
+      $base[] = $data;
+    }
+    fclose($h);
+//        kint($base);
+//        foreach ($base as $value){
+//          kint($value[10]);
+//        }
+//        die();
+    $head = array_shift($base);
+    $tag     = array();
+    $fonte   = array();
+    $tema    = array();
+    $tipo    = array();
+    $divisao = array();
+    $cont = 0;
+    foreach($base as $value) {
+      $files   = explode('#', $value[8]);
+      foreach($files as $record){
+        $filename = $filePath . "\arquivos-para-migrar\mapoteca&#92" . $record;
+        $file = file_save_data($filename, 'public://' . $record, FILE_EXISTS_REPLACE);
+      }
+      $node = Node::create(['type' => 'mapas_prontos']);
+      $node->set('title', $value[1]);
+      $body = [
+        'value' => $value[5],
+        'format' => 'basic_html',
+      ];
+      $node->set('body', $body);
+      $node->set('uid', 1);
+      $documentos[] = [
+        'target_id' => $file->id(),
+        'description' => $value[7],
+      ];
+      $node->set('field_documento', $documentos);
+      $files = array();
+      $node->set('field_documento_data_ref', $value[9]);
+      $node->set('field_documento_data_lancamento', $value[10]);
+      //        $tags = [118, 123, 122];
+      
+//      $tags     = explode('#', $value[4]);
+      $fontes   = explode('#', $value[3]);
+      $temas    = explode('#', $value[2]);
+      $tipos    = explode('#', $value[6]);
+      $divisoes = explode('#', $value[4]);
+      
+//      foreach($tags as $value){
+//        $tag[] = getTidByName($value, 'tags_base_dados');
+//      }
+      foreach($fontes as $value){
+        $fonte[] = getTidByName($value, 'fontes_mapas_prontos');
+      }
+      foreach($temas as $value){
+        $tema[] = getTidByName($value, 'temas_mapas_prontos');
+      }
+      foreach($tipos as $value){
+        $tipo[] = getTidByName($value, 'tipos_mapas_prontos');
+      }
+      foreach($divisoes as $value){
+        $divisao[] = getTidByName($value, 'divisao_territorial_do_brasil');
+      }
+//      $node->set('field_documento_tags', $tag);
+      $node->set('field_documento_fontes', $fonte);
+      $node->set('field_documento_temas', $tema);
+      $node->set('field_documento_tipos', $tipo);
+      $node->set('field_documento_divisao', $divisao);
+      
+      //        $node->set('field_documento_data_lancamento', '1981-01-17');
+      $tag   = array();
+      $fonte = array();
+      $tema  = array();
+      $tipo  = array();
+      $node->status = 1;
+      $node->langcod = 'pt-br';
+      $node->enforceIsNew();
+      $node->save();
+      $cont++;
+    }
+    drupal_set_message("foram registrados " . $count . "nodes!\n");
+    
+    return [
+      '#type' => 'markup',
+      '#markup' => $this->t('Importação realizada com sucesso!'),
+    ];
+    
+  }
   
   }
 
