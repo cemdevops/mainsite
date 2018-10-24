990155793be8f9cd418dbdd9560616f287998bd8
DanielMM <medeiros.danielm@gmail.com>
2018-10-24 20:35:52 -0200

    changes

--- themes/custom/cem/assets/js-dev/_home-video.js
+++ themes/custom/cem/assets/js-dev/_home-video.js
@@ -1,7 +1,8 @@
 (function(window, document, $) {
     'use strict';
     if ($('.home-video').length) {
-        $('.home-video div.home-video-box div.inner').slick({
+        // $('.home-video div.home-video-box div.inner').slick({
+        $('.home-video div.home-video-box div.inner>div.row>div.views-element-container>div').slick({
             autoplay: true,
             autoplaySpeed: 6000,
             dots: true,
