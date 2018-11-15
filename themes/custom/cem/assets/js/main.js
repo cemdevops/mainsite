1cc35f8b0982eab634750abad2a98a6ec9d0e68f
DanielMM <medeiros.danielm@gmail.com>
2018-11-14 22:50:09 -0200

    css update

--- themes/custom/cem/assets/js/main.js
+++ themes/custom/cem/assets/js/main.js
@@ -1,4 +1,17 @@
 (function(window, document, $) {
+  "use strict";
+  if ($(".accordion").length) {
+    $(".accordion .title").on("click", this, function() {
+      console.log(this);
+
+      $(this)
+        .parent(".accordion")
+        .toggleClass("open");
+    });
+  }
+})(window, document, jQuery);
+
+(function(window, document, $) {
     'use strict';
     if ($('.agenda-slider').length) {
         $('.agenda-slider>.inner').slick({
@@ -36,6 +49,23 @@
             ]
         });
 
+    }
+})(window, document, jQuery);
+(function(window, document, $) {
+    'use strict';
+    if ($('.evento-fotos-slider-item').length) {
+        //$('.home-publications div.home-publications-box>div.inner').slick({
+        // $('.home-pesquisadores div.home-pesquisadores-box>div.row').slick({
+        $('.evento-slider-foto-container div.evento-fotos-slider-box>div.row>div.views-element-container>div').slick({
+            autoplay: true,
+            autoplaySpeed: 6000,
+            dots: true,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 5,
+            slidesToScroll: 5,
+            arrows: false
+        });
     }
 })(window, document, jQuery);
 (function (window, document, $) {
@@ -137,7 +167,7 @@
         var $btnPlus = $('.incricao-convites .number .plus');
         var $btnLow = $('.incricao-convites .number .low');
 
-
+        
         $btnPlus.on('click',function(){
             conviteField.attr('value', ++conviteQtd);
         });
@@ -148,7 +178,6 @@
             conviteField.attr('value', --conviteQtd);
         });
     }
-    
 })(window, document, jQuery);
 (function(window, document, $) {
   "use strict";
