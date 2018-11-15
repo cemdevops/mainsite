d5d50244bb005d271b79751ff9afb66d04f54a60
DanielMM <medeiros.danielm@gmail.com>
2018-11-15 12:59:55 -0200

    changes

--- modules/custom/csv_import/src/Controller/publicacoes_importController.php
+++ modules/custom/csv_import/src/Controller/publicacoes_importController.php
@@ -27,34 +27,37 @@ class publicacoes_importController extends ControllerBase {
         }
 
         $filePath = \Drupal::service('file_system')->realpath(file_default_scheme() . "://");
-        $publicacoes = $filePath . "/publicacoes.csv";
+        $publicacoes = $filePath . "/pay-load-publish-pt.csv";
         $h =fopen($publicacoes, "r");
 
-        while (($data = fgetcsv($h, 100000, ";")) !== FALSE) {
+        while (($data = fgetcsv($h, 100000, "|")) !== FALSE) {
             $base[] = $data;
         }
         fclose($h);
+        kint($base);
+        exit();
         $head  = array_shift($base);
         $autor = array();
         $count = 0;
         foreach($base as $value) {
 
-            $files       = explode('#', $value[8]);
+            $files = explode('#', $value[6]);
             $description = explode('#', $value[7]);
-            if(count($files) != count($description)){
+            if (count($files) != count($description)) {
                 echo "A quantidade de descrição não está igual a quantiade de arquivos: linha: " . $count;
-            }{
+            }
+            {
                 $file_entity = array_combine($files, $description);
             }
             $count++;
             $node = Node::create(['type' => 'publicacoes']);
-            $documentos  = array();
+            $documentos = array();
 
-            foreach($file_entity as $file => $descricao){
+            foreach ($file_entity as $file => $descricao) {
 
-                $file_source = $filePath . "/arquivos-para-migrar/publicacoes/" . $file;
+                $file_source = $filePath . "/publicacoes-migrated-files-mari/" . $file;
 
-                if(file_exists($file_source) && is_file($file_source)) {
+                if (file_exists($file_source) && is_file($file_source)) {
                     $uri = file_unmanaged_copy($file_source, 'public://' . $file, FILE_EXISTS_REPLACE);
                     $files = File::Create(['uri' => $uri]);
                     $files->save();
@@ -64,17 +67,17 @@ class publicacoes_importController extends ControllerBase {
                     ];
                     $node->set('field_publicacoes_arquivo', $documentos);
                 }
-                if(file_exists($file_source)) {
+                if (file_exists($file_source)) {
                     $log = $filePath . '/logs/arquivos-faltantes-publicacoes.txt';
                     $current = file_get_contents($log);
                     $current .= "{$file}\n";
                     file_put_contents($log, $current);
                 }
             }
-            if(!empty($value[1])){
+            if (!empty($value[1])) {
                 $node->set('title', $value[1]);
-            }else{
-                $logs = $filePath .'/title.txt';
+            } else {
+                $logs = $filePath . '/title.txt';
                 $currents = file_get_contents($logs);
                 file_put_contents($logs, "");
                 $currents .= "{$value[1]}\n";
@@ -82,39 +85,25 @@ class publicacoes_importController extends ControllerBase {
                 $node->set('title', 'SEM TITULO');
             }
             $body = [
-                'value' => $value[5],
+                'value' => $value[2],
                 'format' => 'full_html',
             ];
             $node->set('body', $body);
 
-			$field_infor = [
-                'value' => $value[5],
-                'format' => 'full_html',
+            $node->set('field_ano_de_publicacao', $value[3]);
+
+            $link = [
+                'uri' => $value[4],
+                'title' => $value[5],
             ];
-            $node->set('field_infor', $field_infor);
-            $node->set('uid', 1);
+            $node->set('field_publicacoes_link', $link);
 
-            $node->set('field_ano_de_publicacao', $value[9]);
-            $node->set('field_data_de_publicacao', $value[10]);
-            $node->set('field_publicacoes_link', $value[10]);
-            $node->set('field_nome_da_revista', $value[10]);
-            
-            $autor     = explode('#', $value[4]);
-            $categoria = explode('#', $value[4]);
-        
-            foreach($autor as $value){
-                $autor[] = getTidByName($value, 'autor');
-            }
-
-            foreach($categoria as $value){
-                $categoria[] = getTidByName($value, 'categoria_publicacoes');
-            }
+            $categoria = getTidByName($value[1], 'categoria_publicacoes');
 
             $node->set('field_publicacoes_categoria', $categoria);
-            $node->set('field_publicacoes_autor', $autor);
-            $categoria = array();
-            $autor     = array();
+            $node->set('field_publicacoes_autores', $value[8]);
 
+            $node->set('uid', 1);
             $node->status = 1;
             $node->langcod = 'pt-br';
             $node->enforceIsNew();
