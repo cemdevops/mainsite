f77418f6515b1563b171a51e45715e639105b6d3
Gustavo Martusewicz <gustavp_crj@hotmail.com>
2018-11-25 23:00:57 -0200

    verify if drop down item is a current page

--- themes/custom/cem/assets/js/main.js
+++ themes/custom/cem/assets/js/main.js
@@ -201,6 +201,22 @@
     }
 
 })(window, document, jQuery);
+(function (window, document, $) {
+    'use strict';
+
+
+    if ($('.quem-somos-menu').length) {
+        console.log($('.quem-somos-menu li.active').parent().parent().is('li'));
+        
+        if ( $('.quem-somos-menu li.active').parent().parent().is('li') ){
+            $('.quem-somos-menu li.active').parent().parent().addClass('active')
+        }
+        else{
+            console.log('errou');
+            
+        }
+    }
+})(window, document, jQuery);
 (function(window, document, $) {
   "use strict";
   $(".quem-somos-conteudo-col .field_pos_doutorandos").on("click", function() {
