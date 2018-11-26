9583866dca39fe5e3147c79086695c16d086f310
Gustavo Martusewicz <gustavp_crj@hotmail.com>
2018-11-25 22:38:12 -0200

    add new 'mapoteca' page

--- themes/custom/cem/assets/js/main.js
+++ themes/custom/cem/assets/js/main.js
@@ -14,7 +14,7 @@
 (function(window, document, $) {
     'use strict';
     if ($('.agenda-slider').length) {
-        $('.agenda-slider>.inner').slick({
+        $('.agenda-slider>.inner div.views-element-containe > div').slick({
             dots: false,
             infinite: true,
             speed: 400,
@@ -178,6 +178,28 @@
             conviteField.attr('value', --conviteQtd);
         });
     }
+})(window, document, jQuery);
+(function (window, document, $) {
+    'use strict';
+
+    if ($('.download-item.with-map').length) {
+        var $mapItem = $('.download-item.with-map');
+
+        $mapItem.each(function(){
+            $(this).children('.texto').clone().addClass('textoHide').addClass('hide').removeClass('texto').insertBefore($(this).children('.texto'));
+
+            var $text = $(this).children('.texto').text().substring(0,250)+'...';
+            $(this).children('.texto').text($text);
+            console.log($text);
+
+            $(this).children('.btn-show-more').on('click',function(){
+                $(this).toggleClass('show');
+                $(this).parent('div').children('.texto').toggleClass('hide');
+                $(this).parent('div').children('.textoHide').toggleClass('hide');
+            });
+        });
+    }
+
 })(window, document, jQuery);
 (function(window, document, $) {
   "use strict";
