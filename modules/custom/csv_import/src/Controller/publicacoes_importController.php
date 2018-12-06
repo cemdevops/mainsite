23f3306554e35dd483c720d4a80d2048882a5e45
DanielMM <medeiros.danielm@gmail.com>
2018-12-05 23:25:45 -0200

    csv publicacoes

--- modules/custom/csv_import/src/Controller/publicacoes_importController.php
+++ modules/custom/csv_import/src/Controller/publicacoes_importController.php
@@ -27,7 +27,7 @@ class publicacoes_importController extends ControllerBase {
         }
 
         $filePath = \Drupal::service('file_system')->realpath(file_default_scheme() . "://");
-        $publicacoes = $filePath . "/csv_file_pt-br.csv";
+        $publicacoes = $filePath . "/part1_others_publications-pt-br-single-rows.csv";
         $h =fopen($publicacoes, "r");
         $base = [];
         while (($data = fgetcsv($h, 10000, "|")) !== FALSE) {
@@ -41,77 +41,55 @@ class publicacoes_importController extends ControllerBase {
         $autor = array();
         $count = 0;
         foreach($base as $value) {
+            $file = $value[6];
+            $description = $value[7];
 
-            if($value[1] != 'Livro'):
-                $files = explode('#', $value[6]);
-                $description = explode('#', $value[7]);
-                if (count($files) != count($description)) {
-                    echo "A quantidade de descrição não está igual a quantiade de arquivos: linha: " . $count;
-                }
-                {
-                    $file_entity = array_combine($files, $description);
-                }
-                $count++;
-                $node = Node::create(['type' => 'publicacoes']);
-                $documentos = array();
+            $count++;
+            $node = Node::create(['type' => 'publicacoes']);
+            $documentos = array();
+            $file_source = $filePath . "/publicacoes-migrated-files-mari/" . $file;
 
-                foreach ($file_entity as $file => $descricao) {
-
-                    $file_source = $filePath . "/publicacoes-migrated-files-mari/" . $file;
-
-                    if (file_exists($file_source) && is_file($file_source)) {
-                        $uri = file_unmanaged_copy($file_source, 'public://' . $file, FILE_EXISTS_REPLACE);
-                        $files = File::Create(['uri' => $uri]);
-                        $files->save();
-                        $documentos[] = [
-                            'target_id' => $files->id(),
-                            'description' => $descricao,
-                        ];
-                        $node->set('field_publicacoes_arquivo', $documentos);
-                    }
-                    if (file_exists($file_source)) {
-                        $log = $filePath . '/logs/arquivos-faltantes-publicacoes.txt';
-                        $current = file_get_contents($log);
-                        $current .= "{$file}\n";
-                        file_put_contents($log, $current);
-                    }
-                }
-                if (!empty($value[1])) {
-                    $node->set('title', $value[1]);
-                } else {
-                    $logs = $filePath . '/title.txt';
-                    $currents = file_get_contents($logs);
-                    file_put_contents($logs, "");
-                    $currents .= "{$value[1]}\n";
-                    file_put_contents($logs, $currents);
-                    $node->set('title', 'SEM TITULO');
-                }
-                $body = [
-                    'value' => $value[2],
-                    'format' => 'full_html',
+            if (file_exists($file_source) && is_file($file_source)) {
+                $uri = file_unmanaged_copy($file_source, 'public://' . $file, FILE_EXISTS_REPLACE);
+                $files = File::Create(['uri' => $uri]);
+                $files->save();
+                $documentos = [
+                    'target_id' => $files->id(),
+                    'description' => $description,
                 ];
-                $node->set('body', $body);
+                $node->set('field_publicacoes_arquivo', $documentos);
+            }
+            if (!empty($value[1])) {
+                $node->set('title', $value[1]);
+            } else {
+                $node->set('title', 'SEM TITULO');
+            }
+            $body = [
+                'value' => $value[2],
+                'format' => 'full_html',
+            ];
+            $node->set('body', $body);
+            $node->set('field_ano_de_publicacao', $value[3]);
 
-                $node->set('field_ano_de_publicacao', $value[3]);
-
-                $link = [
-                    'uri' => $value[4],
-                    'title' => $value[5],
-                ];
+            $link = [
+                'uri' => $value[4],
+                'title' => $value[5],
+            ];
+            if (!empty($value[4])) {
                 $node->set('field_publicacoes_link', $link);
+            }
+            $categoria = getTidByName($value[1], 'categoria_publicacoes');
 
-                $categoria = getTidByName($value[1], 'categoria_publicacoes');
+            $node->set('field_publicacoes_categoria', $categoria);
+            $node->set('field_publicacoes_autores', $value[8]);
 
-                $node->set('field_publicacoes_categoria', $categoria);
-                $node->set('field_publicacoes_autores', $value[8]);
-
-                $node->set('uid', 1);
-                $node->status = 1;
-                $node->langcod = 'pt-br';
-                $node->enforceIsNew();
-                $node->save();
-            endif;
+            $node->set('uid', 1);
+            $node->status = 1;
+            $node->langcod = 'pt-br';
+            $node->enforceIsNew();
+            $node->save();
         }
+
         drupal_set_message("Foram registrados" . $count .  " nodes!\n");
         return [
             '#type' => 'markup',
